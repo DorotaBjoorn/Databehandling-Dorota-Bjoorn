32fd341d54d1a45a81bb0d72d1ff3b379cb58008
Dorota Bjoorn <Dorota.Bjoorn@iths2022>
2022-11-09 17:36:05 +0100

    plot_nan_colls function added

--- Excercises/functions.py
+++ Excercises/functions.py
@@ -1,3 +1,9 @@
+def plot_nan_colls(df):
+    nan_columns = df.columns[df.isna().any()].to_list()
+    sum_nuns = df.isna().sum().to_list()
+    nan_columns_values = [i for i in sum_nuns if i > 0]
+    plt.bar(nan_columns, nan_columns_values)
+
+
 def remove_legend_heading(fig):
     fig.update_layout(legend={'title_text':''})
-    
